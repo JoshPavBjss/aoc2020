5769b5221434d81448c3351526c0f79564e742fc
JoshPavBjss <josh.paveley@bjss.com>
2020-12-08 10:20:49 +0000

    Day 8 done, needs to be cleaned up.

--- days/day8/day8.go
+++ days/day8/day8.go
@@ -2,6 +2,9 @@ package days
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 
 	"../../shared"
 )
@@ -9,14 +12,95 @@ import (
 // Day8Computer solves day8
 type Day8Computer struct{}
 
+type instructionMap map[int]instruction
+
+type instruction struct {
+	instructionType string
+	value           int
+}
+
+func parseInstruction(line string) instruction {
+	split := strings.Split(line, " ")
+	parsedValue, _ := strconv.Atoi(split[1])
+	return instruction{instructionType: split[0], value: parsedValue}
+}
+
+func buildInstructionMap(input shared.Input) instructionMap {
+	instructions := make(instructionMap)
+	for i, line := range input {
+		instructions[i] = parseInstruction(line)
+	}
+	return instructions
+}
+
+func getValueAtLoopStart(instructions instructionMap) (int, error) {
+	accumulator := 0
+	i := 0
+
+	visited := make(map[int]struct{})
+
+	for i < len(instructions) {
+		currentInstruction := instructions[i]
+		if _, ok := visited[i]; ok {
+			return accumulator, nil
+		}
+		visited[i] = struct{}{}
+
+		if currentInstruction.instructionType == "acc" {
+			accumulator += currentInstruction.value
+		} else if currentInstruction.instructionType == "jmp" {
+			i += currentInstruction.value
+			continue
+		}
+		i++
+	}
+	return accumulator, errors.New("Should not have got here")
+}
+
 // Part1 of day 8
 func (d *Day8Computer) Part1(input shared.Input) (shared.Result, error) {
+
+	instructions := buildInstructionMap(input)
+
+	fmt.Println(getValueAtLoopStart(instructions))
 
 	return "", errors.New("Not yet implemented")
 }
 
+func createInstructionMapCopy(instructions instructionMap) instructionMap {
+	newInstructions := make(instructionMap)
+	for k, v := range instructions {
+		newInstructions[k] = v
+	}
+	return newInstructions
+}
+
+func getSwitchedInstructionType(oldInstruction instruction) instruction {
+	newInstructionType := "jmp"
+	if oldInstruction.instructionType == "jmp" {
+		newInstructionType = "nop"
+	}
+	return instruction{instructionType: newInstructionType, value: oldInstruction.value}
+}
+
 // Part2 of day 8
 func (d *Day8Computer) Part2(input shared.Input) (shared.Result, error) {
+
+	instructions := buildInstructionMap(input)
+
+	for k, v := range instructions {
+		if v.instructionType == "nop" || v.instructionType == "jmp" {
+			newInstructions := createInstructionMapCopy(instructions)
+			newInstructions[k] = getSwitchedInstructionType(v)
+
+			val, err := getValueAtLoopStart(newInstructions)
+
+			if err != nil {
+				fmt.Println("This one worked, acc was:", val)
+				break
+			}
+		}
+	}
 
 	return "", errors.New("Not yet implemented")
 }
--- days/main.go
+++ days/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"time"
 
-	today "../days/day7"
+	today "../days/day8"
 	shared "../shared"
 )
 
@@ -24,9 +24,9 @@ func runForDay(day shared.Day, input shared.Input) {
 
 func main() {
 
-	day := 7
+	day := 8
 
 	fmt.Println("AoC 2020 Day", day)
 
-	runForDay(&today.Day7Computer{}, shared.Input(shared.ReadStringLines(day)))
+	runForDay(&today.Day8Computer{}, shared.Input(shared.ReadStringLines(day)))
 }
