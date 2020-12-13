435e94516898936eb90ad8d760a72bf78fcd798d
JoshPavBjss <josh.paveley@bjss.com>
2020-12-13 21:20:24 +0000

    Part 1 cleanup

--- days/day13/day13.go
+++ days/day13/day13.go
@@ -11,19 +11,40 @@ import (
 // Day13Computer solves day13
 type Day13Computer struct{}
 
+type busSchedule struct {
+	busNo  int
+	offset int
+}
+
+func createBusSchedule(input shared.Input) []busSchedule {
+	busSchedules := make([]busSchedule, 0)
+
+	for i, bus := range strings.Split(input[1], ",") {
+		if busNo, err := strconv.Atoi(bus); err == nil {
+			busSchedules = append(busSchedules, busSchedule{busNo: busNo, offset: i})
+		}
+	}
+	return busSchedules
+}
+
+func busDepartsAtTime(busNo, currentTime int) bool {
+	return currentTime%busNo == 0
+}
+
 // Part1 of day 10
 func (d *Day13Computer) Part1(input shared.Input) (shared.Result, error) {
 
 	earliestDepartTime, _ := strconv.Atoi(input[0])
-	busesInService := shared.ToIntSlice(strings.Split(strings.Replace(input[1], ",x", "", -1), ","))
+
+	busesInService := createBusSchedule(input)
 
 	currentTime := earliestDepartTime
 
 	for true {
 
 		for _, bus := range busesInService {
-			if currentTime%bus == 0 {
-				return strconv.Itoa((currentTime - earliestDepartTime) * bus), nil
+			if busDepartsAtTime(bus.busNo, currentTime) {
+				return strconv.Itoa((currentTime - earliestDepartTime) * bus.busNo), nil
 			}
 		}
 		currentTime++
@@ -32,12 +53,7 @@ func (d *Day13Computer) Part1(input shared.Input) (shared.Result, error) {
 	return "", errors.New("Could not find available bus")
 }
 
-type busDepart struct {
-	busNo  int
-	offset int
-}
-
-func earliestBusDepart(busTimes []busDepart, start, increment int) int {
+func earliestBusDepart(busTimes []busSchedule, start, increment int) int {
 
 	timeStamp := start
 
@@ -56,7 +72,7 @@ func earliestBusDepart(busTimes []busDepart, start, increment int) int {
 	return -1
 }
 
-func getIncrement(buses []busDepart) int {
+func getIncrement(buses []busSchedule) int {
 	increment := 1
 	for _, bus := range buses {
 		increment *= bus.busNo
@@ -69,19 +85,19 @@ func (d *Day13Computer) Part2(input shared.Input) (shared.Result, error) {
 
 	buses := strings.Split(input[1], ",")
 
-	busDeparts := make([]busDepart, 0)
+	busSchedules := make([]busSchedule, 0)
 
 	for i, bus := range buses {
 		if busNo, err := strconv.Atoi(bus); err == nil {
-			busDeparts = append(busDeparts, busDepart{busNo: busNo, offset: i})
+			busSchedules = append(busSchedules, busSchedule{busNo: busNo, offset: i})
 		}
 	}
 
-	startTime := busDeparts[0].busNo
+	startTime := busSchedules[0].busNo
 
-	for i := range busDeparts {
+	for i := range busSchedules {
 
-		startTime = earliestBusDepart(busDeparts[0:i+1], startTime, getIncrement(busDeparts[0:i]))
+		startTime = earliestBusDepart(busSchedules[0:i+1], startTime, getIncrement(busSchedules[0:i]))
 	}
 
 	return strconv.Itoa(startTime), nil
